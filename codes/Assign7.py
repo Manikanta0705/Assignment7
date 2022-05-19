a210c3977d559c95c6cf4d9554a92927569e09a0
Manikanta0705 <102511255+Manikanta0705@users.noreply.github.com>
2022-05-19 22:47:55 +0530

    Create Assign7.py

---
+++ codes/Assign7.py
@@ -0,0 +1,10 @@
+"""A die marked 1, 2, 3 in red and 4, 5, 6 in green is tossed. Let A be the event, 'the number is even,' and B be the event,
+'the number is red'. Are A and B independent?"""
+p_A = 1/2
+p_B = 1/2
+p_AB = 1/6
+
+if p_E * p_F == p_EF:
+    print("E and F are independent events")  
+else:
+    print("E and F are not independent events")
